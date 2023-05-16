9bddbd806c75e43cd1d776bf616f9a73fc205d9a
Kminwoo97 <kmo971130@gmail.com>
2023-05-16 14:51:31 +0900

    임의로 부여한 시크릿 키

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,6 +1,9 @@
 package com.example.jwt;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +13,12 @@ class JwtApplicationTests {
     void contextLoads() {
     }
 
+    @Value("${custom.jwt.secretKey}")
+    private String secretKey;
+
+    @Test
+    @DisplayName("secretKey 키가 존재해야한다.")
+    void t1() {
+        Assertions.assertThat(secretKey).isNotNull();
+    }
 }
