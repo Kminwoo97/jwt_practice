b4b34bcc543478212022df19f6b2d23ab5fb4593
Kminwoo97 <kmo971130@gmail.com>
2023-05-16 15:27:17 +0900

    JWT 토큰 발급및 인증

--- src/main/java/com/example/jwt/jwt/JwtProvider.java
+++ src/main/java/com/example/jwt/jwt/JwtProvider.java
@@ -1,11 +1,16 @@
 package com.example.jwt.jwt;
 
+import com.example.jwt.util.Ut;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 @Component //스프링 빈으로 등록
 public class JwtProvider {
@@ -23,6 +28,44 @@ public class JwtProvider {
         if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
 
         return cachedSecretKey;
+    }
+
+    //토큰 발급
+    public String genToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Ut.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    //토큰 확인
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    //토큰안의 데이터 가져오기
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+
+        return Ut.json.toMap(body);
     }
 
 }
--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -55,5 +57,53 @@ class JwtApplicationTests {
         SecretKey secretKey2 = jwtProvider.getSecretKey();
 
         assertThat(secretKey1 == secretKey2).isTrue();
+    }
+
+    @Test
+    @DisplayName("accessToken 을 얻는다.")
+    void t5() {
+        //토큰안에 넣을 데이터 세팅
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        // 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+        System.out.println("accessToken : " + accessToken);
+
+        assertThat(accessToken).isNotNull();
+    }
+
+    @Test
+    @DisplayName("accessToken 을 통해서 claims 를 얻을 수 있다.")
+    void t6() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        // 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+        System.out.println("accessToken : " + accessToken);
+
+        //토큰이 유효한지 확인
+        assertThat(jwtProvider.verify(accessToken)).isTrue();
+
+        Map<String, Object> claimsFromToken = jwtProvider.getClaims(accessToken);
+        System.out.println("claimsFromToken : " + claimsFromToken);
+    }
+
+    @Test
+    @DisplayName("accessToken 을 통해서 claims 를 얻을 수 있다.")
+    void t7() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+
+        // 토큰의 유효시간을 -1 로 설정한다.
+        String accessToken = jwtProvider.genToken(claims, -1);
+        System.out.println("accessToken : " + accessToken);
+
+        //토큰이 유효하지 않은지 확인한다.
+        assertThat(jwtProvider.verify(accessToken)).isFalse();
     }
 }
